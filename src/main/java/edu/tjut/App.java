5eda26dadff46e88a1dc51ee5be864f53bee703d
Administrator <Administrator@DESKTOP-CCLG5LU>
2019-03-27 08:17:59 +0800

    my 1st commit

---
+++ src/main/java/edu/tjut/App.java
@@ -0,0 +1,19 @@
+package edu.tjut;
+import java.util.Locale;
+import com.github.javafaker.Faker;
+
+public class App {
+	
+	static Faker faker = new Faker(new Locale("zh-CN"));
+
+	public static void main(String[] args) {
+		
+		
+		
+		Student s1 = new Student(1, faker.name().fullName(), faker.address().fullAddress());
+		
+		System.out.print(s1);
+
+	}
+
+}
